89bb182830db18f6560b38744dd91c61424eb562
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2018-09-03 00:03:53 +0200

    Overhauled calculator program

--- python intro/calculator/calc.py
+++ python intro/calculator/calc.py
@@ -1,49 +1,41 @@
-def main():
-    var1 = input("Number One: ")
-    var2 = input("Number Two: ")
-    var3 = input("What would you like to calculate? ")
+result = float
+success = bool
 
-    sum = float(var1) + float(var2)
-    product = float(var1) * float(var2)
+def mainFunc ():
+    var1 = input("Input First Number: ")
+    var2 = input("Input Second Number: ")
+    calcType = input("What do you want to calculate for? ")
 
-    if var3 != "sum" and var3 != "difference" and var3 != "product" and var3 != "quotient":
-        print("Please calculate sum, difference, product or quotient.")
-
-    if var3 == "sum":
-        print(sum)
-
-    if var3 == "difference":
-        if var1 > var2:
-            difference = float(var1) - float(var2)
-        else:
-            difference = float(var2) - float(var1)
-
-        print(difference)
-
-    if var3 == "product":
-        print(product)
-
-    if var3 == "quotient":
-        if var1 > var2:
-            quotient = float(var1) / float(var2)
-        else:
-            quotient = float(var2) / float(var1)
-
-        print(quotient)
-
-    option()
-
-
-
-def option():
-    var4 = input("Would you like to try more? ")
-
-    if var4 == "yes":
-        main()
-
+    if calcType != "sum" and calcType != "product" and calcType != "difference" and calcType != "quotient":
+        print("Try using keywords; sum, product, difference or quotient.")
+        success = 0
     else:
-        exit
+        success = 1
 
-main()
+    if calcType == "sum":
+        result = float(var1) + float(var2)
 
+    if calcType == "product":
+        result = float(var1) * float(var2)
 
+    if calcType == "difference":
+        result = float(var1) - float(var2)
+
+    if calcType == "quotient":
+        result = float(var1) / float(var2)
+
+    if success == 1:
+        print(result)
+        retry()
+    else:
+        retry()
+
+def retry ():
+    var3 = input("Try again? ")
+
+    if var3 == "yes":
+        mainFunc()
+    else:
+        exit()
+
+mainFunc()
