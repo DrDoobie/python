4a359aad04b0f40e04a0f6ae76e57e39922afdff
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2018-09-02 18:59:41 +0200

    More calculator function

--- python intro/calculator/calc.py
+++ python intro/calculator/calc.py
@@ -1,30 +1,44 @@
-var1 = input("Number One: ")
-var2 = input("Number Two: ")
-var3 = input("What would you like to calculate? ")
+def main():
+    var1 = input("Number One: ")
+    var2 = input("Number Two: ")
+    var3 = input("What would you like to calculate? ")
 
-sum = float(var1) + float(var2)
-product = float(var1) * float(var2)
+    sum = float(var1) + float(var2)
+    product = float(var1) * float(var2)
 
-if var3 == "sum":
-    print(sum)
+    if var3 != "sum" or var3 != "difference" or var3 != "product" or var3 != "quotient":\
+        print("Please calculate sum, difference, product or quotient.")
 
-if var3 == "difference":
-    if var1 > var2:
-        difference = float(var1) - float(var2)
+    if var3 == "sum":
+        print(sum)
+
+    if var3 == "difference":
+        if var1 > var2:
+            difference = float(var1) - float(var2)
+        else:
+            difference = float(var2) - float(var1)
+
+        print(difference)
+
+    if var3 == "product":
+        print(product)
+
+    if var3 == "quotient":
+        if var1 > var2:
+            quotient = float(var1) / float(var2)
+        else:
+            quotient = float(var2) / float(var1)
+
+        print(quotient)
+
+    var4 = input("Would you like to calculate more? ")
+
+    if var4 == "yes":
+        main()
+
     else:
-        difference = float(var2) - float(var1)
+        exit
 
-    print(difference)
-
-if var3 == "product":
-    print(product)
-
-if var3 == "quotient":
-    if var1 > var2:
-        quotient = float(var1) / float(var2)
-    else:
-        quotient = float(var2) / float(var1)
-
-    print(quotient)
+main()
 
 
