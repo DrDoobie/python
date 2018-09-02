f8a4e00f45dc1074dc3bf2709e1a03cba7cd6b6e
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2018-09-03 00:24:05 +0200

    Dice roller finished

--- python intro/dice rolling sim/dice.py
+++ python intro/dice rolling sim/dice.py
@@ -1,1 +1,13 @@
-# dice roll sim
+def main ():
+    import random
+    dice = random.randint(1, 6)
+    print("Result: ", dice)
+
+    input1 = input("Roll again? ")
+
+    if input1 == "yes":
+        main()
+    else:
+        exit
+
+main()
