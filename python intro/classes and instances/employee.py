7ac393a661b12cecaf0d90f836f28359caeb3d87
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2018-09-03 12:49:20 +0200

    Employee database example

---
+++ python intro/classes and instances/employee.py
@@ -0,0 +1,27 @@
+class Employee:
+        annualSalInc = 1.04
+
+        def __init__ (self, last, first, salary):
+            self.last = last
+            self.first = first
+            self.salary = float(salary * 1000)
+            self.email = last + "-" + first + "@company.com"
+            self.promotionsReceived = 0
+
+        def pullFullName (self):
+            return "{}, {}".format(self.last, self.first)
+
+        def upgradeSalary (self):
+            self.salary *= 1.04
+            self.promotionsReceived += 1
+
+# Employee light here:
+emp_1 = Employee("Hendrix", "Jimi", 100)
+emp_2 = Employee("Lennon", "John", 200)
+
+# Use this function to pull all of employee's information
+def pullEmpInfo (emp):
+    print(emp.__dict__)
+
+pullEmpInfo(emp_1)
+pullEmpInfo(emp_2)
