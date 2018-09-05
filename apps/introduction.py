addcd4c98ddb9992e1f1ce11b03be0469ef75d3f
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2018-09-05 18:49:25 +0200

    Added buttons
    Added buttons to app intro

--- apps/introduction.py
+++ apps/introduction.py
@@ -1,11 +1,24 @@
 from tkinter import *
 # Notes from first time working with tkinter
+# Pack is used to quickly place something in the window
 
-# Instantiates window
+# Instantiates window - REQUIRED
 root = Tk()
-# Label = text(window, string)
-label = Label(root, text = "I am a string.")
-# Quickly place widget using pack
-label.pack()
-# Keeps the window going/open. When this loop is broken the window is closed.
+
+# "Container" in window root
+topFrame = Frame(root)
+topFrame.pack()
+
+botFrame = Frame(root)
+# Parameter inside of pack allows you to define where you want the widget
+botFrame.pack(side = BOTTOM)
+
+# Simple button (location, display, colour(optional))
+button1 = Button(topFrame, text = "Click Me!", fg = "red")
+button1.pack(side = LEFT)
+
+button2 = Button(topFrame, text = "Click Me!", fg = "blue")
+button2.pack(side = RIGHT)
+
+# Keeps the window going/open. When this loop is broken the window is closed. - REQUIRED
 root.mainloop()
