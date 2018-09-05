bb1cadcf1d28393e4ec5014796ec8487809a72b0
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2018-09-05 15:05:16 +0200

    Tile class

--- python intro/text adventure/tiles.py
+++ python intro/text adventure/tiles.py
@@ -1,2 +1,35 @@
+import enemies, items
 
+class Tile:
+    def __init__ (self, x, y):
+        self.x = x
+        self.y = y
+
+    def enterTileNotif (self):
+        raise NotImplementedError()
+
+    def modifyPlayer (self, player):
+        raise NotImplementedError()
+
+class StartRoom (Tile):
+    def enterTileNotif (self):
+        return "Test"
+
+    def modifyPlayer (self, player):
+        # No action
+        pass
+
+class LootRoom (Tile):
+    def __init__ (self, x, y, item):
+        self.item = item
+        super().__init__(x, y)
+
+    def addLoot (self, player):
+        player.inventory.append(self.item)
+
+    def modifyPlayer (self, player):
+        self.addLoot(player)
+
+class EnemyRoom (Tile):
+    
 
